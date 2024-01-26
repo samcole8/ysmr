4f599ca10f68ae86a17178baf37cbf1d3da4f769
Sam Cole <sam@samcole.me>
2024-01-26 14:29:19 +0000

    Add PoC code for Elasticsearch interaction

--- ysmr/ysmr.py
+++ ysmr/ysmr.py
@@ -1,60 +1,45 @@
-import datetime
 import requests
 import importlib
+from elasticsearch import Elasticsearch
 
 import helpers
 
 CONFIG = "ysmr.conf" # Relative path to config
 
-def parse(ssh_log):
-    """Filter SSH log for relevant data."""
-    payload = {}
-    ssh_log_chron = reversed(ssh_log) # Make log iterable in chronological order
-    for line in ssh_log_chron:
-        # If line is relevant:
-        if "password" in line:
-            # Set payload values
-            if "Accepted" in line:
-                payload_type = "1"
-            else:
-                payload_type = "0"
-            lexemes = line.split(" ")
-            time_split = lexemes[2].split(":")
-            payload["type"] = payload_type
-            payload["year"] = datetime.datetime.now().year
-            payload["day"] = lexemes[1]
-            payload["hour"] = time_split[0]
-            payload["minute"] = time_split[1]
-            payload["second"] = time_split[2]
-            payload["ipv4"] = lexemes[10]
-            payload["name"] = lexemes[8]
-            payload["port"] = lexemes[12]
-            break
-    return payload
-
 def post(url, params):
     post = requests.post(url, params=params)
     print(post)
+
+def fetch(port, host, index):
+    es = Elasticsearch(f"{host}:{port}")
+    query = {
+        "query": {
+            "match_all": {}
+        }
+    }
+    result = es.search(index=index, body=query)
+    for hit in result['hits']['hits']:
+        print(hit['_source'])
+    es.close()
+
 
 def ysmr():
     """Parse SSH log for latest data and post to API."""
     absolute_path = helpers.get_path()
     config = helpers.load_json(absolute_path / CONFIG)
-    # Open log and parse for data.
-    with open(absolute_path / config["ssh_log_path"]) as ssh_log:
-        ssh_log = ssh_log.readlines()
-    payload = parse(ssh_log)
+    # Open log and get data
+    fetch(config["port"], config["host"], config["index"])
     # Post API call for each active module.
-    for module in config["modules"]:
-        if config["modules"][module] == "1":
-            # Import modules temporarily to get API calls.
-            temp_module = importlib.import_module("modules." + module) 
-            call_list = temp_module.wrap(payload)
-            # Post API calls.
-            for call in call_list:
-                url = call[0]
-                params = call[1]
-                post(url, params)
+#    for module in config["modules"]:
+#        if config["modules"][module] == "1":
+#            # Import modules temporarily to get API calls.
+#            temp_module = importlib.import_module("modules." + module)
+#            call_list = temp_module.wrap(payload)
+#            # Post API calls.
+#            for call in call_list:
+#                url = call[0]
+#                params = call[1]
+#                post(url, params)
 
 
 if __name__ == "__main__":
