65080f275afe68d5cf7b979ded44826ecd678c46
Sam Cole <sam@samcole.me>
2024-03-12 15:26:09 +0000

    Create Log and SSHLog class

--- ysmr/ysmr.py
+++ ysmr/ysmr.py
@@ -2,12 +2,26 @@
 
 import toml
 import sys
+import importlib
 
 # Relative path to configuration file
 CONFIG_PATH = "ysmr.toml"
 
+class Log:
+    def __init__(self, timestamp, **kwargs):
+        self.timestamp = timestamp
+        self.other = kwargs
+
+class SSHLog(Log):  # SSHLog inherits from Log
+    def __init__(self, status, ipv4, port, **kwargs):
+        # Call the __init__ method of the base class (Log) using super()
+        super().__init__(**kwargs)
+        self.status = status
+        self.ipv4 = ipv4
+        self.port = port
+
 class Module:
-    def __init__(self, name, **kwargs):
+    def __init__(self, name, enabled, **kwargs):
         self.name = name
         self.settings = kwargs
 
@@ -19,9 +33,14 @@ def load_config():
 
 def ysmr(timestamp, status, ipv4, port):
     """Pass parameters to notification modules."""
+    log = SSHLog(timestamp=timestamp, status=status, ipv4=ipv4, port=port)
     config = load_config()
     # Create list of enabled Module instances
     modules = [Module(**module) for module in config.get('module', []) if module.get('enabled', False)]
+    for module in modules:
+        mod = importlib.import_module(module.name)
+        mod.run(module.settings, log)
+        
 
 if __name__ == "__main__":
     ysmr(sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[4])
