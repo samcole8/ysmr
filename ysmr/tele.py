7e19f2d1a5d6231b72811770ee392c6ebe963ad2
Sam Cole <sam@samcole.me>
2024-03-12 15:56:50 +0000

    Create tele.py

--- ysmr/tele.py
+++ ysmr/tele.py
@@ -1,2 +1,5 @@
+import requests
+
 def run(settings, log):
-    
+    message=f"{log.status} login at {log.timestamp} from {log.ipv4} on port {log.port}.\n"
+    requests.post(f"https://api.telegram.org/bot{settings['bot_token']}/sendMessage", params={"chat_id": settings["chat_id"], "text": message},)
