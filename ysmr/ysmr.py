71be11cd8095382094354632059efdb16550fa5b
Sam Cole <sam@samcole.me>
2024-03-12 14:51:20 +0000

    Add filter to list comprehension

--- ysmr/ysmr.py
+++ ysmr/ysmr.py
@@ -7,9 +7,8 @@ import sys
 CONFIG_PATH = "ysmr.toml"
 
 class Module:
-    def __init__(self, name, enabled, **kwargs):
+    def __init__(self, name, **kwargs):
         self.name = name
-        self.enabled = enabled
         self.settings = kwargs
 
 def load_config():
@@ -21,8 +20,8 @@ def load_config():
 def ysmr(timestamp, status, ipv4, port):
     """Pass parameters to notification modules."""
     config = load_config()
-    # Create list of Module instances
-    modules = [Module(**module) for module in config.get('module', [])]
+    # Create list of enabled Module instances
+    modules = [Module(**module) for module in config.get('module', []) if module.get('enabled', False)]
 
 if __name__ == "__main__":
     ysmr(sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[4])
