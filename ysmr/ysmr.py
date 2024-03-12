ef2731c10825beb42c6d520604303633a18baf0e
Sam Cole <sam@samcole.me>
2024-03-12 14:17:43 +0000

    Add code to load config file

--- ysmr/ysmr.py
+++ ysmr/ysmr.py
@@ -1,3 +1,20 @@
 """Handle inputs from logstash and pass them to notification modules."""
 
-import sy
+import toml
+import sys
+
+# Relative path to configuration file
+CONFIG_PATH = "ysmr.toml"
+
+def load_config():
+    """Open config and return dictionary"""
+    with open(CONFIG_PATH, "r") as f:
+        config = toml.load(f)
+    return config
+
+def ysmr():
+    """Pass parameters to notification modules."""
+    config = load_config()
+
+if __name__ == "__main__":
+    ysmr()
