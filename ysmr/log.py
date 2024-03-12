bf17a5b2e476e81fc2b6cefa964487e4a694df32
Sam Cole <sam@samcole.me>
2024-03-12 15:45:58 +0000

    Remove stray print from log.py

--- ysmr/log.py
+++ ysmr/log.py
@@ -1,4 +1,3 @@
 def run(settings, log):
     with open("ysmr.log", "a") as f:
-        print(log.timestamp)
         f.write(f"{log.timestamp} | {log.status} login from {log.ipv4} on port {log.port}.\n")
---
+++ ysmr/tele.py
@@ -0,0 +1,2 @@
+def run(settings, log):
+    
