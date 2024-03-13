7029b2f6ee7d72f83e4e848d899d765b68af8249
Sam Cole <sam@samcole.me>
2024-03-13 09:41:59 +0000

    Remove whitespace

--- ysmr/ysmr.py
+++ ysmr/ysmr.py
@@ -32,8 +32,6 @@ def load_config(path):
         # Create list of enabled Module instances
         module_configs = [ModuleConfig(**module_config) for module_config in toml.load(f).get('module', []) if module_config.get('enabled', False)]
     return module_configs
-    
-
 
 def ysmr(timestamp, status, ipv4, port):
     """Pass parameters to notification modules."""
