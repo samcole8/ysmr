61889867b44ac5463b73b0efce6475a9cd036d48
Sam Cole <sam@samcole.me>
2024-03-14 07:47:55 +0000

    Utilise get_msg method of log object

--- ysmr/log.py
+++ ysmr/log.py
@@ -3,5 +3,4 @@
 def run(settings, log):
     """Write log message to local filesystem."""
     with open("ysmr.log", "a") as f:
-        f.write(f"{log.timestamp} | {log.status} "
-                f"login from {log.ipv4} on port {log.port}.\n")
+        f.write(log.get_msg())
