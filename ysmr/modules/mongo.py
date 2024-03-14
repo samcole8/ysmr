1047e9eb18468378aa068834d4a8d3a63629165f
Sam Cole <sam@samcole.me>
2024-03-14 14:06:06 +0000

    Create empty modules

---
+++ ysmr/modules/mongo.py
@@ -0,0 +1,9 @@
+"""MongoDB notification module for ysmr.
+
+This module accepts the log and config objects created by ysmr.
+
+Using API parameters provided in the config object, the log data is
+wrapped in an API call and sent to MongoDB accordingly.
+
+This module is packaged by default with ysmr.
+"""
