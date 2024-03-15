bba5fd249ed1497280ef0fb8564fba4a4c37a374
Sam Cole <sam@samcole.me>
2024-03-15 12:55:55 +0000

    Implement data sending

--- ysmr/modules/mongo.py
+++ ysmr/modules/mongo.py
@@ -7,3 +7,18 @@ wrapped in an API call and sent to MongoDB accordingly.
 
 This module is packaged by default with ysmr.
 """
+
+from pymongo import MongoClient
+
+
+def run(conf, log):
+    """Send MongoDB notification."""
+    # Create document from log data
+    document = {}
+    for key, value in vars(log).items():
+        document[key] = value
+    # Open MongoDB connection and post data
+    with MongoClient(conf.url) as client:
+        db = client[conf.db]
+        collection = db[conf.collection]
+        collection.insert_one(document)
