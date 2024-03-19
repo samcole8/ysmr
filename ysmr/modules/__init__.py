3b5149afaea5b934aa62616d1e1710205cd58424
Sam Cole <sam@samcole.me>
2024-03-19 16:48:47 +0000

    Modify Module superclass

--- ysmr/modules/__init__.py
+++ ysmr/modules/__init__.py
@@ -7,14 +7,14 @@ from dataclasses import dataclass
 class Module:
     """Module superclass."""
 
-    def __init__(self, log, conf):
+    def __init__(self, log, instances):
         """Initialise Module class."""
         self.log = log
         # Create Instance objects from conf
         self.instances = [
-            self.Instance(**inst_data)
-            for inst_data in conf["instance"]
-            if inst_data.get("enabled")
+            self.Instance(**instance)
+            for instance in instances
+            if instance.get("enabled")
         ]
 
     @dataclass
@@ -22,3 +22,13 @@ class Module:
         """Parent Instance class to be inherited."""
 
         pass
+
+    def notify(self):
+        """Wrap notification code in error check."""
+        for instance in self.instances:
+            try:
+                instance.go(self.log)
+            except AttributeError as e:
+                print(f"Check YourModule.send() for errors: {e}")
+            except Exception as e:
+                print(f"Exception occured: {e}")
