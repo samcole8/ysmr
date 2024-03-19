252c368031895c3c3b7d7b9aba945e212947aceb
Sam Cole <sam@samcole.me>
2024-03-19 14:19:24 +0000

    Add documentation

--- ysmr/modules/template.py
+++ ysmr/modules/template.py
@@ -5,15 +5,26 @@
 # Required imports
 from . import Module, dataclass
 
+
 class MyModule(Module):
+    """Custom module class.
+
+    This class can store attributes and send methods for custom modules. You
+    can rename it.
+
+    It must contain at least an `Instance` class and `send` method, as below.
+    """
 
     @dataclass
     class Instance(Module.Instance):
+        """Dataclass for defining configuration attributes."""
+
         # Define your attributes here
         my_attribute_1: str
         my_attribute_2: int
         my_attribute_3: bool
 
     def send(self):
+        """Deposit/send data in the specified method."""
         # Add your notification code here
         pass
